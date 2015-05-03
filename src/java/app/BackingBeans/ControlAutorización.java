2d94152ca80c042054f930a2888bcc8ac34e565b
LaHind <hind_lab@hotmail.fr>
2015-05-03 17:02:40 +0200

    ControlAutorización

---
+++ src/java/app/BackingBeans/ControlAutorización.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package app.BackingBeans;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+/**
+ *
+ * @author hii
+ */
+@ManagedBean
+@RequestScoped
+public class ControlAutorización {
+
+    private Usuario user;
+    
+    public ControlAutorización() {
+    }
+
+    public Usuario getUser() {
+        return user;
+    }
+
+    public void setUser(Usuario user) {
+        this.user = user;
+    }
+    
+    public String PáginaPrincipal()
+    {
+        if (user == null) 
+        {
+            return "index.xhtml";
+        } 
+        else 
+        { /////// PIENSO QUITAR EL CONTROL DE SI ES PROFESIONAL O USUARIO DEL OTRO BACKING BEAN Y PONERLO AQUI
+    }
+    
+}
