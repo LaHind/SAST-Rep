37c31affc4a73b5e640a2fd39dbec484510d0f3f
LaHind <hind_lab@hotmail.fr>
2015-05-03 13:15:08 +0200

    Prueba

---
+++ src/java/app/BackingBeans/LogIn.java
@@ -0,0 +1,88 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package app.BackingBeans;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+/**
+ *
+ * @author hii
+ */
+@ManagedBean // HAVE A LOOK
+@RequestScoped
+public class LogIn {
+    
+   private String usuario;
+   private String password;
+   private String estado;
+   private List<Usuario> usuarios;
+   private List<Profesional> profesionales;  
+   @inject //
+
+
+    public LogIn() {
+        usuarios=new ArrayList<Usuario>;
+        profesionales=new ArrayList<Profesional>;
+        for(Usuarios u:usuarios)
+        {
+            usuarios.add(u);
+        }
+        for(Profesional p:profesionales)
+        {
+            profesionales.add(p);
+        }
+        
+    }
+    
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String nombre) {
+        estado = nombre;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+    public String Autenticar()
+    {
+        for(Usuario u:usuarios)
+        {
+            if(usuario.getNif().equals(usuario))
+            {
+                if(u.getPassword()==password)  // Hay que añadir
+                {
+                    //autenticar (con controlador)
+                }
+                else
+                {
+                    //contraseña errónea 
+                }
+                
+            }
+            else 
+            {
+                //Usuario no encontrado
+            }
+        }
+    }
+    
+}
